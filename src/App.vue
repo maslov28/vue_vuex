e3bfce81121837ec970f846b0f3cae39efa61df0
maslov28 <artem.fatefulchoice@mail.ru>
2022-07-04 00:13:22 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header class="header"></header>
+    <div class="container">
+      <h1 class="title">Товары</h1>
+      <div class="cards">
+        <app-card v-for="card in cards" :key="card.id"></app-card>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppCard from './components/AppCard.vue'
+import {reactive} from 'vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    AppCard
+  },
+  setup() {
+    const cards = reactive([
+      {
+        id: 1,
+        name: 'Solar Guitars A2.7TBR',
+        price: 72990,
+        description: '7-струнная электрогитара, цвет красный',
+        img: 'https://mirm.ru/info/img_400/CNT75960.jpg.webp'
+      },
+      {
+        id: 2,
+        name: '',
+        price: 100,
+        description: '',
+        img: ''
+      },
+      {
+        id: 3,
+        name: '',
+        price: 100,
+        description: '',
+        img: ''
+      },
+      {
+        id: 4,
+        name: '',
+        price: 100,
+        description: '',
+        img: ''
+      },
+      {
+        id: 5,
+        name: '',
+        price: 100,
+        description: '',
+        img: ''
+      },
+    ])
+
+    return {
+      cards
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .header {
+    background-color: rgb(223, 223, 223);
+    width: 100%;
+    height: 50px;
+    margin-bottom: 50px;
+  }
+  .container{
+    width: 1000px;
+    margin: 0 auto;
+  }
+  .title {
+    text-align: center;
+  }
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 </style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <div class="card__info">
+      <img class="card__img" src="https://mirm.ru/info/img_400/CNT75960.jpg.webp" alt="">
+      <h3 class="card__title">Solar Guitars A2.7TBR</h3>
+      <p class="card__desc">7-струнная электрогитара, цвет красный</p>
+      <p class="card__price">72 990 р.</p>
+    </div>
+    <div class="card__buttons">
+      <button>-</button>
+      <button>+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .card {
+    max-width: 200px;
+    border: 1px solid rgba(0, 0, 0, 0.267);
+    border-radius: 10px;
+    padding: 20px;
+    margin: 20px;
+  }
+  .card__img {
+    max-width: 200px;
+  }
+  .card__price {
+    text-align: right;
+  }
+  .card__buttons{
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
